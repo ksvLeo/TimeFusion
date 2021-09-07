ac8f798aec50b22ec32135fa64357e75229a84aa
Enzo Yanes <enzo.yanes@fusionit.uy>
2021-09-07 19:35:26 -0300

    API - Get referrers #1057 Fix - namespace of ClientDto

--- src/Application/Clients/Queries/GetClientsQuery.cs
+++ src/Application/Clients/Queries/GetClientsQuery.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using FusionIT.TimeFusion.Application.Clients.Dtos;
 using FusionIT.TimeFusion.Application.Common.Interfaces;
-using FusionIT.TimeFusion.Clients.Dtos;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -11,28 +11,28 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace FusionIT.TimeFusion.Application.Customers.Queries
+namespace FusionIT.TimeFusion.Application.Clients.Queries
 {
-    public class GetCustomersQuery : IRequest<List<ClientDto>>
+    public class GetClientsQuery : IRequest<List<ClientDto>>
     {
-        public class GetCustomersQueryHandle : IRequestHandler<GetCustomersQuery, List<ClientDto>>
+        public class GetClientsQueryHandle : IRequestHandler<GetClientsQuery, List<ClientDto>>
         {
             private readonly IApplicationDbContext _context;
             private readonly IMapper _mapper;
 
-            public GetCustomersQueryHandle(IApplicationDbContext context, IMapper mapper)
+            public GetClientsQueryHandle(IApplicationDbContext context, IMapper mapper)
             {
                 _context = context;
                 _mapper = mapper;
             }
 
-            public async Task<List<ClientDto>> Handle(GetCustomersQuery request, CancellationToken cancellationToken)
+            public async Task<List<ClientDto>> Handle(GetClientsQuery request, CancellationToken cancellationToken)
             {
-                List<ClientDto> customers = await _context.Clients
+                List<ClientDto> clients = await _context.Clients
                 .ProjectTo<ClientDto>(_mapper.ConfigurationProvider)
                 .ToListAsync();
 
-                return customers;
+                return clients;
             }
         }
     }
